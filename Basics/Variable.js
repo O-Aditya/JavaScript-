e1a7c2b570febadfe8bcb194c6bdf4cc7602707d
Aditya Maurya <mauryaaditya7405@gmail.com>
2024-05-15 22:44:46 +0530

    chore: add Variable.js with account details and console table

---
+++ Basics/Variable.js
@@ -0,0 +1,15 @@
+const accountId = 25426
+let accountEmail = "Eddi@gmail.com"
+var accountPassword = "30301020256"
+accountAddress = "New York, USA"
+
+//const is a constant variable, it can't be changed
+console.log(accountId)
+
+//let is a variable that can be changed
+
+accountEmail = "Aditya@icloud.com"
+accountPassword = "5456875"
+accountAddress = "New Delhi, India"
+
+console.table([accountEmail, accountPassword, accountAddress])
