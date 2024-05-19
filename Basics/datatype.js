e2144f12d3315614ae588eef10e173c4f2294030
Aditya Maurya <mauryaaditya7405@gmail.com>
2024-05-19 18:39:01 +0530

    feat: Add examples of non-primitive data types in JavaScript

--- Basics/datatype.js
+++ Basics/datatype.js
@@ -23,3 +23,39 @@ console.log("Data type is " + (typeof nullVar));
 // 5. Undefined
 let undef = undefined;
 console.log("Data type is " + (typeof undef));
+
+
+// Non-primitive data type
+// 1. Arrays
+let myarr = [1, 2, 3, 4, false, "string"];
+console.log("Data type is " + (typeof myarr));
+console.log(myarr);
+
+// 2. Object literals
+let stMarks = {
+    harry: 89,
+    Shubham: 36,
+    Rohan: 34
+}
+console.log(stMarks);
+
+
+// 3. Functions
+function findName() {
+    
+}
+console.log("Data type is " + (typeof findName));
+
+// 4. Dates
+let date = new Date();
+console.log("Data type is " + (typeof date));
+console.log(date);
+
+// 5. Regular Expressions
+let re = /harry/;
+console.log("Data type is " + (typeof re));
+
+// 6. Symbol
+let sym = Symbol();
+console.log("Data type is " + (typeof sym));
+
