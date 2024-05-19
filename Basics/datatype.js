4a81c6ac737f0dfe4ecd899e90999a3dce4abfe7
Aditya Maurya <mauryaaditya7405@gmail.com>
2024-05-19 18:34:51 +0530

    feat: Add datatype.js with examples of primitive data types in JavaScript

---
+++ Basics/datatype.js
@@ -0,0 +1,25 @@
+//datatype in javascript 
+// 1. Primitive data type
+// 2. Non-primitive data type
+
+// Primitive data type
+// 1. String
+let user_name = "harry";
+console.log("My string is " + user_name);
+console.log("Data type is " + (typeof user_name));
+
+// 2. Numbers
+let marks = 34;
+console.log("Data type is " + (typeof marks));
+
+// 3. Boolean
+let isDriver = true;
+console.log("Data type is " + (typeof isDriver));
+
+// 4. Null
+let nullVar = null; // standalone value
+console.log("Data type is " + (typeof nullVar));
+
+// 5. Undefined
+let undef = undefined;
+console.log("Data type is " + (typeof undef));
