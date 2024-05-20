708278cf7aae34f3d1f5152122cc2c8ab77eed17
Aditya Maurya <mauryaaditya7405@gmail.com>
2024-05-20 14:35:31 +0530

    chore: Add math.js with examples of math functions in JavaScript

---
+++ Basics/math.js
@@ -0,0 +1,22 @@
+// MATH  in JAvascript
+
+// console.log(Math.PI);
+// console.log(Math);
+
+//function of math
+
+// console.log(Math.round(4.7)); // round off
+
+// console.log(Math.pow(2, 3)); // 2^3
+
+// console.log(Math.sqrt(16)); // square root of 16
+
+// console.log(Math.abs(-4.7)); // absolute value
+
+// console.log(Math.ceil(4.4)); // round up
+
+// console.log(Math.floor(4.9)); // round down
+
+// console.log(Math.max(5,8));  // maximum value
+
+console.log(Math.round(Math.random()*10)); // random number between 0 and 1
