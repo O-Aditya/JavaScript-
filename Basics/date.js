abb7fe89bbebbd8df5d7e13a89f6100d1d8994c3
Aditya Maurya <mauryaaditya7405@gmail.com>
2024-05-22 22:42:05 +0530

    Add date.js with examples of date manipulation in JavaScript

---
+++ Basics/date.js
@@ -0,0 +1,19 @@
+let date = new Date();
+console.log(date); // 2021-07-07T07:00:00.000Z
+
+console.log(date.toString()); // Wed Jul 07 2021 12:30:00 GMT+0530 (India Standard Time)
+
+console.log(date.toDateString()); // Wed Jul 07 2021
+
+console.log(date.toTimeString()); // 12:30:00 GMT+0530 (India Standard Time)
+
+console.log(date.toLocaleString()); // 7/7/2021, 12:30:00 PM
+
+console.log(date.getFullYear()); // 2024
+
+console.log(date.getMonth()); // 6
+
+console.log(date.getDate()); // 7
+
+console.log(date.getDay()); // 3
+
