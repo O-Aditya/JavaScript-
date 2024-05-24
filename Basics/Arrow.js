d56bb8efadefacd314c04b0e60f9e23f181f71d1
Aditya Maurya <mauryaaditya7405@gmail.com>
2024-05-24 22:02:58 +0530

    Refactor Arrow.js to use an IIFE function for better encapsulation

--- Basics/Arrow.js
+++ Basics/Arrow.js
@@ -11,5 +11,12 @@ const demo = () => {
 }
 demo() // window object
 
+//point to be noted
+//Arrow function does not have its own this. It always refers to the parent object.
+//Arrow function does not have arguments object. It uses the arguments object of its parent function.
+//Arrow function does not have prototype property.
+//Arrow function cannot be used as a constructor.
+
+
 
 
